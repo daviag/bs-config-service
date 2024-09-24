0a1046f5bd3589ef0e23d5721e545e3b53f9da9c
David V. <villarejod@gmail.com>
2024-09-24 09:38:35 +0200

    First config

---
+++ src/test/java/com/daviag/bookshop/configservice/BsConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daviag.bookshop.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
